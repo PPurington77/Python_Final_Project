61236dd98e6fe7952365b5cf42bb33637f2a4004
PPurington77 <papurington@gmail.com>
2022-07-21 13:35:13 -0600

    files done

---
+++ server.py
@@ -0,0 +1,8 @@
+from flask_app import app
+#remember to add controller files as you build them... ex below
+# from flask_app.controllers import controller_users
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
